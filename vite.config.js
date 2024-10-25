80f3e5deea915bdf415a71a4b7aaaa0c222aa8f7
Pham Tien Dat <tdat9663@gmail.com>
2024-10-25 21:49:48 +0700

    init project #2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'./' // dung de deploy, sever de hieu duong dan hon, co the them o package.json
 })
