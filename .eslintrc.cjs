9c5bbda4ecc352d7654e2832658105ec523a2378
Pham Tien Dat <tdat9663@gmail.com>
2024-10-27 23:44:02 +0700

    Add more Rules to eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,37 @@
-// Updated by trungquandev.com's author on May 13 2023
-// Sample Eslint config for Node.js project
 module.exports = {
   env: { browser: true, es2020: true, node: true },
-  extends: [ 'eslint:recommended' ],
-  parser: '@babel/eslint-parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module', allowImportExportEverywhere: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended'
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: [],
+  plugins: [
+    'react',
+    'react-hooks',
+    'react-refresh'
+  ],
   rules: {
+    //react
+    'react-refresh/only-export-components': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 0,
+    'react/display-name': 0,
+    //mui
+    "no-restricted-imports": [
+      "error",
+      {
+        "patterns": ["@mui/*/*/*"]
+      }
+    ],
+    //common
     'no-console': 1,
     'no-lonely-if': 1,
     'no-unused-vars': 1,
-    'no-trailing-spaces': 1,     
+    'no-trailing-spaces': 1,
     'no-multi-spaces': 1,
     'no-multiple-empty-lines': 1,
     'space-before-blocks': ['error', 'always'],
--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { Button } from "@mui/material";
 import AcUnitIcon from "@mui/icons-material/AcUnit";
 import { pink } from "@mui/material/colors";
 function App() {
+  
   return (
     <>
       <h1>tien dat</h1>
-
       <Button variant="contained">
         <AcUnitIcon sx={{ color: pink[100] }} />
       </Button>
