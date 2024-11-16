a714b658d0c877dc02de0990a92364bf0952664b
Pham Tien Dat <tdat9663@gmail.com>
2024-11-16 21:07:59 +0700

    dark_light_p2

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import CssBaseline from '@mui/material/CssBaseline'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
-import { ThemeProvider } from '@emotion/react'
 import theme from './theme.js'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CssVarsProvider theme={theme}>
