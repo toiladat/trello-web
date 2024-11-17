2ac79af0ef49234900a204ee98fe8b36e621c894
Pham Tien Dat <tdat9663@gmail.com>
2024-11-17 18:48:12 +0700

    basic_layout

--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,10 @@ import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 
 import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
 const theme = extendTheme({
+  trello:{
+    appBarHeight:'48px',
+    boardBarHeigh:'58px'
+  },
   colorSchemes: {
     light: {
       palette: {
