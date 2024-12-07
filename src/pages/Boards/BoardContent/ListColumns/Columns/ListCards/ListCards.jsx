bd97d5127d0aec7d96e7c6814f8bc5a28a8b932a
Pham Tien Dat <tdat9663@gmail.com>
2024-12-07 21:56:40 +0700

    drag_drop_card_p1

--- src/pages/Boards/BoardContent/ListColumns/Columns/ListCards/ListCards.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Columns/ListCards/ListCards.jsx
@@ -1,31 +1,35 @@
 import Box from '@mui/material/Box'
 import Card from './Card/Card'
+import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
 
 function ListCards({ cards }) {
 
   return (
-    <Box
-      sx={{
-        p:'0 5px', //trick scroll bar column
-        m:'0 5px',
-        display:'flex',
-        flexDirection:'column',
-        gap:1,
-        overflowX:'hidden',
-        overflowY:'auto',
-        maxHeight:(theme) => `calc( ${theme.trello.boardContentHeight} - ${theme.spacing(5)} - ${theme.trello.columnHeaderHeigh} - ${theme.trello.columnFooterHeigh})`,
-        '&::-webkit-scrollbar-thumb':{
-          backgroundColor:'#ced0da',
-          borderRadius:'8px'
-        },
-        '&::-webkit-scrollbar-thumb:hover':{
-          backgroundColor:'#bfc2cf'
-        }
-      }}
-    >
-      {cards?.map(card => <Card card={card} key={card._id }/>)}
+    <SortableContext items={ cards?.map(c => c._id) } strategy={verticalListSortingStrategy}>  
+      <Box
+        sx={{
+          p:'0 5px', //trick scroll bar column
+          m:'0 5px',
+          display:'flex',
+          flexDirection:'column',
+          gap:1,
+          overflowX:'hidden',
+          overflowY:'auto',
+          maxHeight:(theme) => `calc( ${theme.trello.boardContentHeight} - ${theme.spacing(5)} - ${theme.trello.columnHeaderHeigh} - ${theme.trello.columnFooterHeigh})`,
+          '&::-webkit-scrollbar-thumb':{
+            backgroundColor:'#ced0da',
+            borderRadius:'8px'
+          },
+          '&::-webkit-scrollbar-thumb:hover':{
+            backgroundColor:'#bfc2cf'
+          }
+        }}
+      >
+        {cards?.map(card => <Card card={card} key={card._id }/>)}
 
-    </Box>
+      </Box>
+    </SortableContext>
+
   )
 }
 
